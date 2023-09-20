ea13b9c3bbe01b3294f9b9df0af83bee594b46ab
Muhammad Atif Ali <me@matifali.dev>
2023-09-21 01:38:20 +0300

    partial matching

--- gcp-region/main.tf
+++ gcp-region/main.tf
@@ -703,7 +703,7 @@ data "coder_parameter" "region" {
   icon         = "/icon/gcp.png"
   mutable      = var.mutable
   dynamic "option" {
-    for_each = { for k, v in local.zones : k => v if(contains(var.default, k) || contains(var.default, "all")) && (!var.gpu_only || v.has_gpu) }
+    for_each = { for k, v in local.zones : k => v if(startswith(k, var.default) || contains(var.default, "all")) && (!var.gpu_only || v.has_gpu) }
     content {
       icon        = try(var.custom_icons[option.key], option.value.icon)
       name        = try(var.custom_names[option.key], option.value.name)
