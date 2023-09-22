d9e1d141ab3513d24b76dcd60a1b039c62b16df6
Stephen Kirby <me@skirby.dev>
2023-09-22 20:23:42 +0000

    satisfied templatefile args

--- git-config/main.tf
+++ git-config/main.tf
@@ -14,16 +14,10 @@ variable "agent_id" {
     description = "The ID of a Coder agent."
 }
 
-# variable "path" {
-#     type = string
-#     description = "The path to a script that will be ran on start enabling a user to personalize their workspace."
-#     default = "~/personalize"
-# }
-
 resource "coder_script" "personalize" {
     agent_id = var.agent_id
-    script = templatefile("${path.module}/run.sh")
+    script = templatefile("${path.module}/run.sh", {})  # TODO: maybe remove templatefile
     display_name = "Git Config"
-    icon = "/emojis/1f58c.png"      # TODO: test if the local git icon works
+    icon = "/emojis/1f58c.png"                          # TODO: test if the local git icon works
     run_on_start = true
 }
