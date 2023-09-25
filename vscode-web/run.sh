d93934a0065101d44d132590fc702125e79779b6
Muhammad Atif Ali <me@matifali.dev>
2023-09-25 16:23:16 +0300

    run apt-get update before apt-get install

--- vscode-web/run.sh
+++ vscode-web/run.sh
@@ -7,7 +7,7 @@ if
   >/dev/null
 then
   printf "$${BOLD}Installing VS Code!\n"
-  output=$(curl -L "https://update.code.visualstudio.com/${VERSION}/linux-deb-x64/stable" -o /tmp/code.deb && sudo apt-get install -y /tmp/code.deb)
+  output=$(curl -L "https://update.code.visualstudio.com/${VERSION}/linux-deb-x64/stable" -o /tmp/code.deb && sudo apt-get update && sudo apt-get install -y /tmp/code.deb)
   if [ $? -ne 0 ]; then
     echo "Failed to install VS Code: $output"
     exit 1
