b6d3e02e6f6d9da5a221b80c7db1eb2dfeb781f1
Muhammad Atif Ali <me@matifali.dev>
2023-09-25 20:11:27 +0300

    fixup!

--- vscode-server/run.sh
+++ vscode-server/run.sh
@@ -5,18 +5,16 @@ BOLD='\033[0;1m'
 # Create install directory if it doesn't exist
 mkdir -p ${INSTALL_DIR}
 
-printf "$${BOLD}Installing vscode-server!\n"
-# Fetch the latest commit hash for stable release
-HASH=$(curl -s https://update.code.visualstudio.com/api/commits/stable/server-linux-x64-web | cut -d '"' -f 2)
+printf "$${BOLD}Installing vscode-cli!\n"
 
-# Download and extract vscode-server tarball
-output=$(curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz && tar -xf vscode_cli.tar.gz -C ${INSTALL_DIR} --strip-components=1 >/dev/null 2>&1)
+# Download and extract code-cli tarball
+output=$(curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz && tar -xf vscode_cli.tar.gz -C ${INSTALL_DIR} && rm vscode_cli.tar.gz)
 
 if [ $? -ne 0 ]; then
-  echo "Failed to install vscode-server: $output"
+  echo "Failed to install vscode-cli: $output"
   exit 1
 fi
-printf "🥳 vscode-server has been installed.\n\n"
+printf "🥳 vscode-cli has been installed.\n\n"
 
 echo "👷 Running ${INSTALL_DIR}/bin/code serve-web --port ${PORT} --without-connection-token --accept-server-license-terms in the background..."
 echo "Check logs at ${LOG_PATH}!"
