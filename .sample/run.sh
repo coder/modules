e446e598fd6c762cc93f337408438befca525e1f
Muhammad Atif Ali <atif@coder.com>
2023-09-26 14:25:42 +0300

    improve .sample

--- .sample/run.sh
+++ .sample/run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 
-echo "Instalalting MODULE_NAME..."
+BOLD='\033[0;1m'
+echo "$${BOLD}Installing MODULE_NAME..."
 # Add code here
 # Use varibles from the templatefile function in main.tf
 # e.g. LOG_PATH, PORT, etc.
 
-echo "Installation comlete!"
+echo "🥳 Installation comlete!"
 
-echo "Starting MODULE_NAME..."
+echo "👷 Starting MODULE_NAME in background..."
 # Start the app in here
 # 1. Use & to run it in background
 # 2. redirct stdout and stderr to log files
 
 ./app >${LOG_PATH} 2>&1 &
 
-echo "MODULE_NAME Started!"
 echo "check logs at ${LOG_PATH}"
