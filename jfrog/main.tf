5e1a96725af030929744a3efd1154178ee25c76a
Muhammad Atif Ali <me@matifali.dev>
2023-09-26 15:52:10 +0300

    fixup!

--- jfrog/main.tf
+++ jfrog/main.tf
@@ -62,6 +62,7 @@ resource "coder_script" "jfrog" {
     JFROG_HOST : var.jfrog_host,
     ARTIFACTORY_USERNAME : artifactory_scoped_token.me.username,
     ARTIFACTORY_ACCESS_TOKEN : artifactory_scoped_token.me.access_token,
+    ARTIFACTORY_EMAIL : data.coder_workspace.me.owner_email,
     REPOSITORY_NPM : lookup(var.package_managers, "npm", ""),
     REPOSITORY_GO : lookup(var.package_managers, "go", ""),
     REPOSITORY_PYPI : lookup(var.package_managers, "pypi", ""),
