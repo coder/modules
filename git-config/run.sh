b32cb3ae9df2b44bab1d00d3e73f6a7d18f4bde1
Stephen Kirby <me@skirby.dev>
2023-09-26 00:33:25 +0000

    echo -> printf

--- git-config/run.sh
+++ git-config/run.sh
@@ -1,32 +1,27 @@
 #!/usr/bin/env sh
 
-# BOLD='\033[0;1m'
+BOLD='\033[0;1m'
 # CODE='\033[36;40;1m'
 # RESET='\033[0m'
-# CODER_EMAIL="${CODER_EMAIL}"
-# CODER_USERNAME="${CODER_USERNAME}"
 
-echo "Running git-config script!\n\n"
+printf "$${BOLD}Checking git-config!\n"
 
 # Check if git is installed
 command -v git >/dev/null 2>&1 || {
-    echo "git is not installed"
+    echo "git is not installed! Install git to sync username and email."
+    exit 1
 }
-
-# Fetch and print user.name and user.email
-git_username=$(git config --get user.name)
-git_useremail=$(git config --get user.email)
 
 # Set git username and email if not set
 if [ -z $(git config --get user.email) ]; then
-    echo "git-config: No user.email found, setting to ${CODER_EMAIL}"
+    printf "git-config: No user.email found, setting to ${CODER_EMAIL}"
     git config --global user.email ${CODER_EMAIL}
 fi
 
 if [ -z $(git config --get user.name) ]; then
-    echo "git-config: No user.name found, setting to ${CODER_USERNAME}"
+    printf "git-config: No user.name found, setting to ${CODER_USERNAME}"
     git config --global user.name ${CODER_USERNAME}
 fi
 
-echo "git-config: using username: $(git config --get user.name)"
-echo "git-config: using email: $(git config --get user.email)"
+printf "$${BOLD}git-config: using username: $(git config --get user.name)"
+printf "$${BOLD}git-config: using email: $(git config --get user.email)"
