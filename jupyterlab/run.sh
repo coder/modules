d8da0514c7defa0e821b3101f84a61513d69e919
Muhammad Atif Ali <me@matifali.dev>
2023-09-26 11:29:29 +0300

    fixup

--- jupyterlab/run.sh
+++ jupyterlab/run.sh
@@ -5,10 +5,10 @@ BOLD='\033[0;1m'
 echo "$${BOLD}Installing jupyterlab!\n"
 
 # check if jupyterlab is installed
-if ! command -v jupyterlab & >/dev/null; then
+if ! command -v jupyterlab > /dev/null 2>&1; then
     # install jupyterlab
     # check if python3 pip is installed
-    if ! command -v pip3 & >/dev/null; then
+    if ! command -v pip3 > /dev/null 2>&1; then
         echo "pip3 is not installed"
         echo "Please install pip3 in your Dockerfile/VM image before running this script"
         exit 1
