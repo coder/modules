8e50651d295957a394acc7d30d35253c1d6dbe7d
Muhammad Atif Ali <me@matifali.dev>
2023-09-26 06:05:28 +0300

    fix sample

--- .sample/run.sh
+++ .sample/run.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env sh
 
-echo "Instalalting ${MODULE_NAME}..."
+echo "Instalalting MODULE_NAME..."
 # Add code here
 # Use varibles from the templatefile function in main.tf
 # e.g. LOG_PATH, PORT, etc.
 
 echo "Installation comlete!"
 
-echo "Starting ${MODULE_NAME}..."
+echo "Starting MODULE_NAME..."
 # Start the app in here
 # 1. Use & to run it in background
 # 2. redirct stdout and stderr to log files
 
 ./app >${LOG_PATH} 2>&1 &
 
-echo "Sample app started!"
+echo "MODULE_NAME Started!"
+echo "check logs at ${LOG_PATH}"
