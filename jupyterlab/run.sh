4c3194b059f1b79a482da9c3ec0283ad2d609cf7
Muhammad Atif Ali <me@matifali.dev>
2023-09-26 11:10:34 +0300

    update run.sh

--- jupyterlab/run.sh
+++ jupyterlab/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-echo "Installing jupyterlab..."
+BOLD='\033[0;1m'
+
+echo "$${BOLD}Installing jupyterlab!\n"
 
 # check if jupyterlab is installed
 if ! command -v jupyterlab & >/dev/null; then
@@ -13,15 +15,11 @@ if ! command -v jupyterlab & >/dev/null; then
     fi
     # install jupyterlab
     pip3 install --user -q -y jupyterlab
-    echo "jupyterlab installed."
+    echo "🥳 jupyterlab has been installed\n\n"
 else
-    echo "jupyterlab is already installed."
+    echo "🥳 jupyterlab is already installed\n\n"
 fi
 
-echo "Starting jupyterlab..."
-
-$HOME/.local/bin/jupyter lab --no-browser --LabApp.token='' --LabApp.password='' >${LOG_PATH} 2>&1 &
-
-echo "jupyterlab Started!"
-
+echo "👷 Starting jupyterlab in background..."
 echo "check logs at ${LOG_PATH}"
+$HOME/.local/bin/jupyter lab --no-browser --LabApp.token='' --LabApp.password='' >${LOG_PATH} 2>&1 &
