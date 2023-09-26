9c4dce96b2d4cad77e9cc442a5ba88935956d031
Muhammad Atif Ali <me@matifali.dev>
2023-09-26 15:48:49 +0300

    cleanup

--- jfrog/main.tf
+++ jfrog/main.tf
@@ -54,7 +54,7 @@ EOF
 resource "coder_script" "jfrog" {
   agent_id     = var.agent_id
   display_name = "jfrog"
-  icon         = local.icon_url
+  icon         = "/icon/jfrog.svg"
   script = templatefile("${path.module}/run.sh", {
     JFROG_HOST : var.jfrog_host,
     ARTIFACTORY_USERNAME : artifactory_scoped_token.me.username,
@@ -65,39 +65,3 @@ resource "coder_script" "jfrog" {
   })
   run_on_start = true
 }
-
-resource "coder_app" "jfrog" {
-  agent_id     = var.agent_id
-  slug         = "jfrog"
-  display_name = "jfrog"
-  url          = "http://localhost:${var.port}"
-  icon         = loocal.icon_url
-  subdomain    = false
-  share        = "owner"
-
-  # Remove if the app does not have a healthcheck endpoint
-  healthcheck {
-    url       = "http://localhost:${var.port}/healthz"
-    interval  = 5
-    threshold = 6
-  }
-}
-
-data "coder_parameter" "jfrog" {
-  type         = "list(string)"
-  name         = "jfrog"
-  display_name = "jfrog"
-  icon         = local.icon_url
-  mutable      = var.mutable
-  default      = local.options["Option 1"]["value"]
-
-  dynamic "option" {
-    for_each = local.options
-    content {
-      icon  = option.value.icon
-      name  = option.value.name
-      value = option.value.value
-    }
-  }
-}
-
