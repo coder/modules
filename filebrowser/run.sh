2111b24f691bf95ee46939c302a92ead88ef3d65
Muhammad Atif Ali <me@matifali.dev>
2023-09-27 17:05:40 +0300

    improve spacing

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env sh
 
 BOLD='\033[0;1m'
-echo "$${BOLD}Installing filebrowser..."
+echo "$${BOLD}Installing filebrowser \n\n"
 
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
 
-echo "🥳 Installation comlete!"
+echo "🥳 Installation comlete! \n\n"
 
-echo "👷 Starting filebrowser in background..."
+echo "👷 Starting filebrowser in background... \n\n"
 
 ROOT_DIR=${FOLDER}
 ROOT_DIR=$${ROOT_DIR/\~/$HOME}
 
-echo "📂 Serving $${ROOT_DIR} at http://localhost:${PORT}"
+echo "📂 Serving $${ROOT_DIR} at http://localhost:${PORT} \n\n"
 
-echo "Running 'filebrowser --noauth --root $ROOT_DIR --port ${PORT}'"
+echo "Running 'filebrowser --noauth --root $ROOT_DIR --port ${PORT}' \n\n"
+
 filebrowser --noauth --root $ROOT_DIR --port ${PORT} >${LOG_PATH} 2>&1 &
 
-echo "📝 Logs at ${LOG_PATH}"
+echo "📝 Logs at ${LOG_PATH} \n\n"
