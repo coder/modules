6338065106f7499a92dabc43a4283c4ac585634e
Stephen Kirby <me@skirby.dev>
2023-09-27 22:07:42 +0000

    trying empty string

--- git-config/main.tf
+++ git-config/main.tf
@@ -26,15 +26,6 @@ variable "allow_email_change" {
   default     = false
 }
 
-# variable "default_username_source" {
-#   type        = string
-#   description = "Default source to use for git-config user.name."
-# }
-
-# variable "default_email_source" {
-#   type        = string
-#   description = "Default source to use for git-config user.email."
-# }
 
 data "coder_workspace" "me" {}
 
@@ -42,7 +33,7 @@ data "coder_parameter" "user_email" {
   count        = var.allow_email_change ? 1 : 0
   name         = "user_email"
   type         = "string"
-  default      = "NONE" #var.default_email_source
+  default      = "" #var.default_email_source
   description  = "Git user.email to be used for commits"
   display_name = "Git config user.email"
   mutable      = true
@@ -52,7 +43,7 @@ data "coder_parameter" "username" {
   count        = var.allow_username_change ? 1 : 0
   name         = "username"
   type         = "string"
-  default      = "NONE" #var.default_username_source
+  default      = ""
   description  = "Git user.name to be used for commits"
   display_name = "Git config user.name"
   mutable      = true
@@ -61,8 +52,8 @@ data "coder_parameter" "username" {
 resource "coder_script" "git_config" {
   agent_id = var.agent_id
   script = templatefile("${path.module}/run.sh", {
-    GIT_USERNAME = data.coder_workspace.me.owner # try(data.coder_parameter.username[0].value, var.default_username_source)
-    GIT_EMAIL    = data.coder_workspace.me.owner_email # try(data.coder_parameter.user_email[0].value, var.default_email_source)
+    GIT_USERNAME = data.coder_parameter.username.value == "" ? data.coder_workspace.me.owner : data.coder_parameter.username.value
+    GIT_EMAIL    = data.coder_parameter.user_email.value == "" ? data.coder_workspace.me.owner_email : data.coder_parameter.user_email.value
   })
   display_name = "Git Config"
   icon         = "/icon/git.svg"
