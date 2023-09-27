99595aa2511d7a01819cb2a3326016267c22afda
Muhammad Atif Ali <me@matifali.dev>
2023-09-27 15:40:29 +0300

    fixup!

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -9,9 +9,8 @@ echo "🥳 Installation comlete!"
 
 echo "👷 Starting filebrowser in background..."
 
-# if FOLDER is ~ then use $HOME
-dir="${FOLDER}"
-dir="$${dir/#~\//$HOME\/}"
-filebrowser --noauth --root $FOLDER --port "${PORT}" >/tmp/filebrowser.log >${LOG_PATH} 2>&1 &
+FOLDER=${FOLDER/\~/$HOME}
+
+filebrowser --noauth --root $FOLDER --port ${PORT} >${LOG_PATH} 2>&1 &
 
 echo "check logs at ${LOG_PATH}"
