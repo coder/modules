c6f9c778228f24c9c71dea96580c3dd4e278c0db
Muhammad Atif Ali <me@matifali.dev>
2023-09-27 14:45:57 +0300

    add filebrowser

---
+++ filebrowser/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = ">= 0.12"
+    }
+  }
+}
+
+# Add required variables for your modules and remove any unneeded variables
+variable "agent_id" {
+  type        = string
+  description = "The ID of a Coder agent."
+}
+
+variable "log_path" {
+  type        = string
+  description = "The path to log filebrowser to."
+  default     = "/tmp/filebrowser.log"
+}
+
+variable "port" {
+  type        = number
+  description = "The port to run filebrowser on."
+  default     = 19999
+}
+
+variable "folder" {
+  type        = string
+  description = "The folder to serve."
+  default     = "~"
+}
+
+resource "coder_script" "filebrowser" {
+  agent_id     = var.agent_id
+  display_name = "filebrowser"
+  icon         = "https://raw.githubusercontent.com/filebrowser/logo/master/icon_raw.svg"
+  script = templatefile("${path.module}/run.sh", {
+    LOG_PATH : var.log_path,
+    PORT : var.port,
+    FOLDER : var.folder,
+  })
+  run_on_start = true
+}
+
+resource "coder_app" "filebrowser" {
+  agent_id     = var.agent_id
+  slug         = "filebrowser"
+  display_name = "filebrowser"
+  url          = "http://localhost:${var.port}"
+  icon         = "https://raw.githubusercontent.com/filebrowser/logo/master/icon_raw.svg"
+  subdomain    = false
+  share        = "owner"
+
+  # Remove if the app does not have a healthcheck endpoint
+  healthcheck {
+    url       = "http://localhost:${var.port}/healthz"
+    interval  = 5
+    threshold = 6
+  }
+}
---
+++ filebrowser/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+BOLD='\033[0;1m'
+echo "$${BOLD}Installing filebrowser..."
+
+curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
+
+echo "🥳 Installation comlete!"
+
+echo "👷 Starting filebrowser in background..."
+
+# if FOLDER is ~ then use $HOME
+if [ "${FOLDER}" = "~" ]; then
+    FOLDER=$HOME
+else
+    FOLDER=${FOLDER}
+fi
+filebrowser --noauth --root $FOLDER --port ${PORT} >/tmp/filebrowser.log >${LOG_PATH} 2>&1 &
+
+echo "check logs at ${LOG_PATH}"
