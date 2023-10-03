5585780952f70897bc6020eb9ca45d3db94f7df8
Muhammad Atif Ali <me@matifali.dev>
2023-10-03 10:30:32 +0300

    fix run.sh formatting

--- .sample/run.sh
+++ .sample/run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 
 BOLD='\033[0;1m'
-printf "$${BOLD}Installing MODULE_NAME..."
+printf "$${BOLD}Installing MODULE_NAME ...\n\n"
 # Add code here
 # Use varibles from the templatefile function in main.tf
 # e.g. LOG_PATH, PORT, etc.
 
-echo "🥳 Installation comlete!"
+printf "🥳 Installation comlete!\n\n"
 
-echo "👷 Starting MODULE_NAME in background..."
+printf "👷 Starting MODULE_NAME in background...\n\n"
 # Start the app in here
 # 1. Use & to run it in background
 # 2. redirct stdout and stderr to log files
 
 ./app >${LOG_PATH} 2>&1 &
 
-echo "check logs at ${LOG_PATH}"
+printf "check logs at ${LOG_PATH} \n\n"
