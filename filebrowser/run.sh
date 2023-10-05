5a8b8763cb488bf78bd455fd873762c222992440
Stephen Kirby <me@skirby.dev>
2023-10-05 21:19:02 +0000

    corrected optional logic

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -12,15 +12,18 @@ printf "👷 Starting filebrowser in background... \n\n"
 ROOT_DIR=${FOLDER}
 ROOT_DIR=$${ROOT_DIR/\~/$HOME}
 
+# Set the database flag if DB_PATH is set
+DB_FLAG=""
 if [ -z "${DB_PATH}" ]; then
   echo "DB_PATH is empty"
-#   DB_PATH=$${ROOT_DIR}/filebrowser.db
+else
+  DB_FLAG="-d ${DB_PATH}"
 fi
 
 printf "📂 Serving $${ROOT_DIR} at http://localhost:${PORT} \n\n"
 
-printf "Running 'filebrowser --noauth --root $ROOT_DIR --port ${PORT}' \n\n" #  -d ${DB_PATH}
+printf "Running 'filebrowser --noauth --root $ROOT_DIR --port ${PORT}$${DB_FLAG}' \n\n" #  -d ${DB_PATH}
 
-filebrowser --noauth --root $ROOT_DIR --port ${PORT} >${LOG_PATH} 2>&1 & #  -d ${DB_PATH} 
+filebrowser --noauth --root $ROOT_DIR --port ${PORT} >${LOG_PATH}$${DB_FLAG} 2>&1 & #  -d ${DB_PATH} 
 
 printf "📝 Logs at ${LOG_PATH} \n\n"
