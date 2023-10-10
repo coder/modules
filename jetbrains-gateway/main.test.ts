c23edf47d8380f151976264432d4c0c74d4c952b
Stephen Kirby <me@skirby.dev>
2023-10-10 18:58:51 +0000

    added jetbrains-gateway test

--- jetbrains-gateway/main.test.ts
+++ jetbrains-gateway/main.test.ts
@@ -1,5 +1,10 @@
-import { describe } from "bun:test";
-import { runTerraformInit, testRequiredVariables } from "../test";
+import { it, expect, describe } from "bun:test";
+import {
+  runTerraformInit,
+  testRequiredVariables,
+  executeScriptInContainer,
+  runTerraformApply,
+} from "../test";
 
 describe("jetbrains-gateway", async () => {
   await runTerraformInit(import.meta.dir);
@@ -9,5 +14,17 @@ describe("jetbrains-gateway", async () => {
     agent_name: "bar",
     folder: "/baz/",
     jetbrains_ides: '["IU", "IC", "PY"]',
+  });
+
+  it("default to first ide", async () => {
+    const state = await runTerraformApply(import.meta.dir, {
+      agent_id: "foo",
+      agent_name: "bar",
+      folder: "/baz/",
+      jetbrains_ides: '["IU", "IC", "PY"]',
+    });
+    expect(state.outputs.jetbrains_ides.value).toBe(
+      '["IU","232.9921.47","https://download.jetbrains.com/idea/ideaIU-2023.2.2.tar.gz"]',
+    );
   });
 });
