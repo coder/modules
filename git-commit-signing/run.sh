396078a6f1f3707dc65c8efc395d51a697682b6d
Phorcys <57866459+phorcys420@users.noreply.github.com>
2023-10-28 22:14:25 +0000

    feat: download keys to ~/.ssh/git-commit-signing

--- git-commit-signing/run.sh
+++ git-commit-signing/run.sh
@@ -15,7 +15,7 @@ if ! command -v jq > /dev/null; then
         exit 1
 fi
 
-mkdir -p ~/.ssh
+mkdir -p ~/.ssh/git-commit-signing
 
 echo "Downloading SSH key"
 
@@ -23,19 +23,19 @@ ssh_key=$(curl --request GET \
         --url "${CODER_AGENT_URL}api/v2/workspaceagents/me/gitsshkey" \
         --header "Coder-Session-Token: ${CODER_AGENT_TOKEN}")
 
-jq --raw-output ".public_key" > ~/.ssh/coder.pub <<EOF
+jq --raw-output ".public_key" > ~/.ssh/git-commit-signing/coder.pub <<EOF
 $ssh_key
 EOF
 
-jq --raw-output ".private_key" > ~/.ssh/coder <<EOF
+jq --raw-output ".private_key" > ~/.ssh/git-commit-signing/coder <<EOF
 $ssh_key
 EOF
 
-chmod -R 400 ~/.ssh/coder
-chmod -R 400 ~/.ssh/coder.pub
+chmod -R 400 ~/.ssh/git-commit-signing/coder
+chmod -R 400 ~/.ssh/git-commit-signing/coder.pub
 
 echo "Configuring git to use the SSH key"
 
 git config --global gpg.format ssh
 git config --global commit.gpgsign true
-git config --global user.signingkey ~/.ssh/coder
+git config --global user.signingkey ~/.ssh/git-commit-signing/coder
