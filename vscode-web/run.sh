ecb98ea1ed09e65ddc2d9a440ae998e440d98a77
Muhammad Atif Ali <me@matifali.dev>
2023-12-16 17:26:52 +0300

    Discard changes to vscode-web/run.sh

--- vscode-web/run.sh
+++ vscode-web/run.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env sh
 
-# shellcheck disable=SC2034
 BOLD='\033[0;1m'
 
-# Convert templated variables to shell variables
-# shellcheck disable=SC2269
-PORT=${PORT}
-LOG_PATH=${LOG_PATH}
-INSTALL_DIR=${INSTALL_DIR}
-
 # Create install directory if it doesn't exist
-mkdir -p "$${INSTALL_DIR}"
+mkdir -p ${INSTALL_DIR}
 
-# shellcheck disable=SC2059
 printf "$${BOLD}Installing vscode-cli!\n"
 
 # Download and extract code-cli tarball
-output=$(curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz && tar -xf vscode_cli.tar.gz -C "$${INSTALL_DIR}" && rm vscode_cli.tar.gz)
+output=$(curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz && tar -xf vscode_cli.tar.gz -C ${INSTALL_DIR} && rm vscode_cli.tar.gz)
 
 if [ $? -ne 0 ]; then
   echo "Failed to install vscode-cli: $output"
@@ -24,5 +16,6 @@ if [ $? -ne 0 ]; then
 fi
 printf "🥳 vscode-cli has been installed.\n\n"
 
-printf "Check logs at %s\n\n" "$${LOG_PATH}"
-"$${INSTALL_DIR}/code" serve-web --port "$${PORT}" --without-connection-token --accept-server-license-terms > "$${LOG_PATH}" 2>&1 &
+echo "👷 Running ${INSTALL_DIR}/bin/code serve-web --port ${PORT} --without-connection-token --accept-server-license-terms in the background..."
+echo "Check logs at ${LOG_PATH}!"
+${INSTALL_DIR}/code serve-web --port ${PORT} --without-connection-token --accept-server-license-terms >${LOG_PATH} 2>&1 &
