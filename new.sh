97a9ff13e06fe137599a67a963f45120d404938d
Muhammad Atif Ali <me@matifali.dev>
2023-12-16 17:26:21 +0300

    Discard changes to new.sh

--- new.sh
+++ new.sh
@@ -7,15 +7,15 @@ MODULE_NAME=$1
 
 # Check if module name is provided
 if [ -z "$MODULE_NAME" ]; then
-  echo "Usage: ./new.sh <module_name>"
-  exit 1
+    echo "Usage: ./new.sh <module_name>"
+    exit 1
 fi
 
 # Create module directory and exit if it alredy exists
 if [ -d "$MODULE_NAME" ]; then
-  echo "Module with name $MODULE_NAME already exists"
-  echo "Please choose a different name"
-  exit 1
+    echo "Module with name $MODULE_NAME already exists"
+    echo "Please choose a different name"
+    exit 1
 fi
 mkdir -p "${MODULE_NAME}"
 
@@ -23,17 +23,17 @@ mkdir -p "${MODULE_NAME}"
 cp -r .sample/* "${MODULE_NAME}"
 
 # Change to module directory
-cd "${MODULE_NAME}" || echo "Failed to change directory to ${MODULE_NAME}" && exit 1
+cd "${MODULE_NAME}"
 
 # Detect OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS
-  sed -i '' "s/MODULE_NAME/${MODULE_NAME}/g" main.tf
-  sed -i '' "s/MODULE_NAME/${MODULE_NAME}/g" README.md
+    # macOS
+    sed -i '' "s/MODULE_NAME/${MODULE_NAME}/g" main.tf
+    sed -i '' "s/MODULE_NAME/${MODULE_NAME}/g" README.md
 else
-  # Linux
-  sed -i "s/MODULE_NAME/${MODULE_NAME}/g" main.tf
-  sed -i "s/MODULE_NAME/${MODULE_NAME}/g" README.md
+    # Linux
+    sed -i "s/MODULE_NAME/${MODULE_NAME}/g" main.tf
+    sed -i "s/MODULE_NAME/${MODULE_NAME}/g" README.md
 fi
 
 # Make run.sh executable
