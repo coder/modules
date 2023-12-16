a60ebcc39de626293ae3d126ed5d38713f9b9f40
Muhammad Atif Ali <me@matifali.dev>
2023-12-16 17:25:29 +0300

    Discard changes to coder-login/run.sh

--- coder-login/run.sh
+++ coder-login/run.sh
@@ -4,17 +4,11 @@
 # logged in to another deployment
 
 BOLD='\033[0;1m'
-RESET='\033[0m'
-
-# Convert templated variables to shell variables
-CODER_DEPLOYMENT_URL=${CODER_DEPLOYMENT_URL}
-CODER_USER_TOKEN=${CODER_USER_TOKEN}
 
 printf "$${BOLD}Logging into Coder...\n\n$${RESET}"
 
-if ! coder list > /dev/null 2>&1; then
-  set +x
-  coder login --token="$${CODER_USER_TOKEN}" --url="$${CODER_DEPLOYMENT_URL}"
+if ! coder list >/dev/null 2>&1; then
+  set +x; coder login --token="${CODER_USER_TOKEN}" --url="${CODER_DEPLOYMENT_URL}"
 else
   echo "You are already authenticated with coder."
 fi
