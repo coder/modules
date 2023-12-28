c289810c784acc9976f13b6f4f1e030032c2e3c4
Muhammad Atif Ali <me@matifali.dev>
2023-12-28 16:51:11 +0300

    use token from `jf rt curl /api/npm/auth`

--- jfrog-oauth/run.sh
+++ jfrog-oauth/run.sh
@@ -33,9 +33,9 @@ email=${ARTIFACTORY_EMAIL}
 registry=${JFROG_URL}/artifactory/api/npm/${REPOSITORY_NPM}
 EOF
   if [ "$(npm --version | cut -d. -f1)" -ge 9 ]; then
-    echo "//${JFROG_HOST}/artifactory/api/npm/${REPOSITORY_NPM}/:_authToken=${ARTIFACTORY_ACCESS_TOKEN}" >> ~/.npmrc
+    echo "//${JFROG_HOST}/artifactory/api/npm/${REPOSITORY_NPM}/:_authToken=$(jf rt curl /api/npm/auth | awk -F'= ' '/_auth =/{print $2}')" >> ~/.npmrc
   else
-    echo "_auth=${ARTIFACTORY_ACCESS_TOKEN}" >> ~/.npmrc
+    echo "_auth=$(jf rt curl /api/npm/auth | awk -F'= ' '/_auth =/{print $2}')" >> ~/.npmrc
     echo "always-auth=true" >> ~/.npmrc
   fi
 fi
