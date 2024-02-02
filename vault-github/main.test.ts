66c648e2ae4469ed088fe83ecb63590b6ba5d7dc
Muhammad Atif Ali <me@matifali.dev>
2024-02-02 23:41:37 +0300

    add tests and simplify module

---
+++ vault-github/main.test.ts
@@ -0,0 +1,11 @@
+import { describe } from "bun:test";
+import { runTerraformInit, testRequiredVariables } from "../test";
+
+describe("vault-token", async () => {
+  await runTerraformInit(import.meta.dir);
+
+  testRequiredVariables(import.meta.dir, {
+    agent_id: "foo",
+    vault_addr: "foo",
+  });
+});
