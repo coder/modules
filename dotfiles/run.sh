f7d948653003eaec32bff70b8cf158d8595a6d9e
Phorcys <57866459+phorcys420@users.noreply.github.com>
2024-03-14 22:17:29 +0100

    chore(dotfiles); fix comments in run.sh

--- dotfiles/run.sh
+++ dotfiles/run.sh
@@ -12,9 +12,9 @@ if [ -n "$${DOTFILES_URI// }" ]; then
   if [ "$DOTFILES_USER" = "$USER" ]; then
     coder dotfiles "$DOTFILES_URI" -y 2>&1 | tee ~/.dotfiles.log
   else
-    # The "eval echo ~$USER" part is used to dynamically get the home directory of the user, see https://superuser.com/a/484280
-    # sudo -u coder sh -c 'eval echo ~$USER' -> "/home/coder"                                                                                               ~/projects/coder-modules
-    # sudo sh -c 'eval echo ~$USER'          -> "/root"
+    # The "eval echo ~$DOTFILES_USER" part is used to dynamically get the home directory of the user, see https://superuser.com/a/484280
+    # sudo -u coder sh -c 'eval echo ~$DOTFILES_USER' -> "/home/coder"
+    # sudo sh -c 'eval echo ~$DOTFILES_USER'          -> "/root"
 
     sudo -u "$DOTFILES_USER" sh -c "$(which coder) dotfiles \"$DOTFILES_URI\" -y 2>&1 | tee -a $(eval echo ~\'$DOTFILES_USER\')/.dotfiles.log"
   fi
