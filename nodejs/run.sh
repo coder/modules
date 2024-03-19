bb61470fa0096b6aafbd58ff2c151eb3570b8065
FlorianGareis <dev@zoker.me>
2024-03-19 10:17:06 +0000

    Set nvm install prefix relativ to $HOME

--- nodejs/run.sh
+++ nodejs/run.sh
@@ -10,7 +10,7 @@ RESET='\033[0m'
 
 printf "$${BOLD}Installing nvm!$${RESET}\n"
 
-export NVM_DIR="$${INSTALL_PREFIX}/nvm"
+export NVM_DIR="$HOME/$${INSTALL_PREFIX}/nvm"
 mkdir -p "$NVM_DIR"
 
 script="$(curl -sS -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$${NVM_VERSION}/install.sh" 2>&1)"
