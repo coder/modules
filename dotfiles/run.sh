0b8fe33b1655d22c65c998f4ecb1d01967b89101
Phorcys <57866459+phorcys420@users.noreply.github.com>
2024-04-14 12:57:19 +0000

    fix(dotfiles): fix formatting

--- dotfiles/run.sh
+++ dotfiles/run.sh
@@ -12,11 +12,12 @@ if [ -n "$${DOTFILES_URI// }" ]; then
   if [ "$DOTFILES_USER" = "$USER" ]; then
     coder dotfiles "$DOTFILES_URI" -y 2>&1 | tee ~/.dotfiles.log
   else
-    # The "eval echo ~$USER" part is used to dynamically get the home directory of the user, see https://superuser.com/a/484280
-    # eval echo ~$USER'                   -> "/home/coder"
-    # eval echo ~${DOTFILES_USER:-root}'  -> "/root""
+    # The `eval echo ~"$DOTFILES_USER"` part is used to dynamically get the home directory of the user, see https://superuser.com/a/484280
+    # eval echo ~coder -> "/home/coder"
+    # eval echo ~root  -> "/root"
 
     CODER_BIN=$(which coder)
     DOTFILES_USER_HOME=$(eval echo ~"$DOTFILES_USER")
-    sudo -u "$DOTFILES_USER" sh -c "'$CODER_BIN' dotfiles '$DOTFILES_URI' -y 2>&1 | tee '$DOTFILES_USER_HOME'/.dotfiles.log"  fi
+    sudo -u "$DOTFILES_USER" sh -c "'$CODER_BIN' dotfiles '$DOTFILES_URI' -y 2>&1 | tee '$DOTFILES_USER_HOME'/.dotfiles.log" fi
+  fi
 fi
