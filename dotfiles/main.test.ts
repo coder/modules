24e50e2bbb497dc282d4dda0e90710305ce7a426
djarbz <30350993+djarbz@users.noreply.github.com>
2024-04-14 09:06:56 -0500

    Dotfiles template default repo (#224)
    Co-authored-by: Muhammad Atif Ali <me@matifali.dev>

--- dotfiles/main.test.ts
+++ dotfiles/main.test.ts
@@ -18,4 +18,13 @@ describe("dotfiles", async () => {
     });
     expect(state.outputs.dotfiles_uri.value).toBe("");
   });
+
+  it("set a default dotfiles_uri", async () => {
+    const default_dotfiles_uri = "foo";
+    const state = await runTerraformApply(import.meta.dir, {
+      agent_id: "foo",
+      default_dotfiles_uri,
+    });
+    expect(state.outputs.dotfiles_uri.value).toBe(default_dotfiles_uri);
+  });
 });
