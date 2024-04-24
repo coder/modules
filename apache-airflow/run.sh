2a30982d1a1dfe3fc0e715a655758557ad54d909
timquinlan <tim@techfocus.net>
2024-04-24 12:43:16 -0400

    Update run.sh added export and scheduler lines

--- apache-airflow/run.sh
+++ apache-airflow/run.sh
@@ -10,6 +10,10 @@ if ! [ -f $filename ] || ! [ -s $filename ]; then
   airflow db init
 fi
 
+export AIRFLOW__CORE__LOAD_EXAMPLES=false
+
 airflow webserver > ${LOG_PATH} 2>&1 &
+
+airflow scheduler >> /tmp/airflow_scheduler.log 2>&1 &
 
 airflow users create -u admin -p admin -r Admin -e admin@admin.com -f Coder -l User
