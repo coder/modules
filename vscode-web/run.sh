b8ebdc911f989fffdf68bd53ca2a96fb502ecae4
Michael Brewer <michael.brewer@gyft.com>
2024-05-02 01:00:25 -0700

    Update vscode-web/run.sh
    Co-authored-by: Muhammad Atif Ali <me@matifali.dev>

--- vscode-web/run.sh
+++ vscode-web/run.sh
@@ -13,7 +13,7 @@ fi
 run_vscode_web() {
   echo "👷 Running $VSCODE_SERVER serve-local --port ${PORT} --host 127.0.0.1 --accept-server-license-terms serve-local --without-connection-token --telemetry-level ${TELEMETRY_LEVEL} in the background..."
   echo "Check logs at ${LOG_PATH}!"
-  "$VSCODE_SERVER" serve-local "$EXTENSION_ARG" --port "${PORT}" --host 127.0.0.1 --accept-server-license-terms serve-local --without-connection-token --telemetry-level "${TELEMETRY_LEVEL}" > "${LOG_PATH}" 2>&1 &
+  "$VSCODE_WEB" serve-local "$EXTENSION_ARG" --port "${PORT}" --host 127.0.0.1 --accept-server-license-terms serve-local --without-connection-token --telemetry-level "${TELEMETRY_LEVEL}" > "${LOG_PATH}" 2>&1 &
 }
 
 # Check if the settings file exists...
