beaa33b6829ef9d35f8cb857fbd3949420d462a5
Asher <ash@coder.com>
2024-05-21 17:21:04 +0000

    Add open_recent option to VS Code desktop (#248)

--- vscode-desktop/main.test.ts
+++ vscode-desktop/main.test.ts
@@ -18,11 +18,53 @@ describe("vscode-desktop", async () => {
       agent_id: "foo",
     });
     expect(state.outputs.vscode_url.value).toBe(
-      "vscode://coder.coder-remote/open?owner=default&workspace=default&token=$SESSION_TOKEN",
+      "vscode://coder.coder-remote/open?owner=default&workspace=default&url=https://mydeployment.coder.com&token=$SESSION_TOKEN",
     );
 
     const resources: any = state.resources;
     expect(resources[1].instances[0].attributes.order).toBeNull();
+  });
+
+  it("adds folder", async () => {
+    const state = await runTerraformApply(import.meta.dir, {
+      agent_id: "foo",
+      folder: "/foo/bar",
+    });
+    expect(state.outputs.vscode_url.value).toBe(
+      "vscode://coder.coder-remote/open?owner=default&workspace=default&folder=/foo/bar&url=https://mydeployment.coder.com&token=$SESSION_TOKEN",
+    );
+  });
+
+  it("adds folder and open_recent", async () => {
+    const state = await runTerraformApply(import.meta.dir, {
+      agent_id: "foo",
+      folder: "/foo/bar",
+      open_recent: true,
+    });
+    expect(state.outputs.vscode_url.value).toBe(
+      "vscode://coder.coder-remote/open?owner=default&workspace=default&folder=/foo/bar&openRecent&url=https://mydeployment.coder.com&token=$SESSION_TOKEN",
+    );
+  });
+
+  it("adds folder but not open_recent", async () => {
+    const state = await runTerraformApply(import.meta.dir, {
+      agent_id: "foo",
+      folder: "/foo/bar",
+      openRecent: false,
+    });
+    expect(state.outputs.vscode_url.value).toBe(
+      "vscode://coder.coder-remote/open?owner=default&workspace=default&folder=/foo/bar&url=https://mydeployment.coder.com&token=$SESSION_TOKEN",
+    );
+  });
+
+  it("adds open_recent", async () => {
+    const state = await runTerraformApply(import.meta.dir, {
+      agent_id: "foo",
+      open_recent: true,
+    });
+    expect(state.outputs.vscode_url.value).toBe(
+      "vscode://coder.coder-remote/open?owner=default&workspace=default&openRecent&url=https://mydeployment.coder.com&token=$SESSION_TOKEN",
+    );
   });
 
   it("expect order to be set", async () => {
