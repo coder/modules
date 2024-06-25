5ec1b207d13d2f2cf20a52e71154ac7d13e61856
Parkreiner <throwawayclover@gmail.com>
2024-06-25 19:58:56 +0000

    docs: remove now-inaccurate comment

--- windows-rdp/devolutions-patch.js
+++ windows-rdp/devolutions-patch.js
@@ -445,9 +445,9 @@ function setupFormOverrides() {
   setupFormDetection();
 }
 
-// Always safe to call setupObscuringStyles immediately because even if the
-// Angular app isn't loaded by the time the function gets called, the CSS will
-// always be globally available for when Angular is finally ready
+// Always safe to call this immediately because even if the Angular app isn't
+// loaded by the time the function gets called, the CSS will always be globally
+// available for when Angular is finally ready
 setupAlwaysOnStyles();
 
 if (document.readyState === "loading") {
