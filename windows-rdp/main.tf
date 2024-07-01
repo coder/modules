83ffef3a7b5fe28a52461ae90169b31bc39a7070
Parkreiner <throwawayclover@gmail.com>
2024-07-01 14:41:28 +0000

    wip: try breaking coder UI on purpose

--- windows-rdp/main.tf
+++ windows-rdp/main.tf
@@ -60,19 +60,9 @@ resource "coder_script" "windows-rdp" {
     $moduleName = "DevolutionsGateway"
     $moduleVersion = "2024.1.5"
 
-    # Install the module with the specified version for all users
-    # This requires administrator privileges
-    try {
-      # Install-PackageProvider is required for AWS. Need to set command to
-      # terminate on failure so that try/catch actually triggers
-      Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ErrorAction Stop
-      Install-Module -Name $moduleName -RequiredVersion $moduleVersion -Force
-    }
-    catch {
-      # If the first command failed, assume that we're on GCP and run
-      # Install-Module only
-      Install-Module -Name $moduleName -RequiredVersion $moduleVersion -Force
-    }
+    # This should cause Google Cloud to break (doing this on purpose)
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ErrorAction Stop
+    Install-Module -Name $moduleName -RequiredVersion $moduleVersion -Force
 
     # Construct the module path for system-wide installation
     $moduleBasePath = "C:\Windows\system32\config\systemprofile\Documents\PowerShell\Modules\$moduleName\$moduleVersion"
