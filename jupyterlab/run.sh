65040e3aba2224d11d9b4f0d3e072845f47e9c61
Muhammad Atif Ali <me@matifali.dev>
2024-09-21 00:20:18 +0500

    Switch from pip3 to pipx for Jupyter install

--- jupyterlab/run.sh
+++ jupyterlab/run.sh
@@ -7,14 +7,14 @@ printf "$${BOLD}Installing jupyterlab!\n"
 # check if jupyterlab is installed
 if ! command -v jupyterlab > /dev/null 2>&1; then
   # install jupyterlab
-  # check if python3 pip is installed
-  if ! command -v pip3 > /dev/null 2>&1; then
-    echo "pip3 is not installed"
-    echo "Please install pip3 in your Dockerfile/VM image before running this script"
+  # check if pipx is installed
+  if ! command -v pipx > /dev/null 2>&1; then
+    echo "pipx is not installed"
+    echo "Please install pipx in your Dockerfile/VM image before running this script"
     exit 1
   fi
   # install jupyterlab
-  pip3 install --upgrade --no-cache-dir --no-warn-script-location jupyterlab
+  pipx install -q jupyterlab
   echo "🥳 jupyterlab has been installed\n\n"
 else
   echo "🥳 jupyterlab is already installed\n\n"
@@ -22,4 +22,4 @@ fi
 
 echo "👷 Starting jupyterlab in background..."
 echo "check logs at ${LOG_PATH}"
-$HOME/.local/bin/jupyter lab --ServerApp.ip='0.0.0.0' --ServerApp.port=${PORT} --no-browser --ServerApp.token='' --ServerApp.password='' > ${LOG_PATH} 2>&1 &
+$HOME/.local/bin/jupyter-lab --ServerApp.ip='0.0.0.0' --ServerApp.port=${PORT} --no-browser --ServerApp.token='' --ServerApp.password='' > ${LOG_PATH} 2>&1 &
