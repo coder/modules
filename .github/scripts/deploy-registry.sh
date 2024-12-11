1d9288a35cd912a8f256675935d5de05a3151949
Benjamin <benjaminpeinhardt@gmail.com>
2024-12-11 16:01:24 -0600

    alright let's try this out

--- .github/scripts/deploy-registry.sh
+++ .github/scripts/deploy-registry.sh
@@ -31,5 +31,3 @@ curl -X POST "https://cloudbuild.googleapis.com/v1/projects/coder-registry-1/tri
 curl -X POST "https://cloudbuild.googleapis.com/v1/projects/coder-registry-1/triggers/http-build-registry-v2-trigger:webhook?key=${GCLOUD_API_KEY}&secret=${GCLOUD_PROD_DEPLOY_SECRET}" \
 -H "Content-Type: application/json" \
 -d '{}'
-
-# Testing now with the secrets set to nonsense strings to make sure they're never logged by the github action.
