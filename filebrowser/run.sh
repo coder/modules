2c304978d11884fdf0a264c0c738173e9a407f54
Dan Mills <dmills@qumulo.com>
2025-02-07 17:25:09 -0800

    Fix filebrowser config

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -11,7 +11,7 @@ fi
 
 printf "🥳 Installation complete! \n\n"
 
-printf "👷 Starting filebrowser in background... \n\n"
+printf "🛠️  Configuring filebrowser \n\n"
 
 ROOT_DIR=${FOLDER}
 ROOT_DIR=$${ROOT_DIR/\~/$HOME}
@@ -21,10 +21,20 @@ if [ "${DB_PATH}" != "filebrowser.db" ]; then
   DB_FLAG=" -d ${DB_PATH}"
 fi
 
+# Check if filebrowser db exists
+if [ ! -f ${DB_PATH} ]; then
+  filebrowser $DB_FLAG config init > ${LOG_PATH} 2>&1
+  filebrowser $DB_FLAG config set  --baseurl ${SERVER_BASE_PATH} --port ${PORT} --auth.method=noauth --root $ROOT_DIR > ${LOG_PATH} 2>&1
+  filebrowser $DB_FLAG users add admin "" --perm.admin=true > ${LOG_PATH} 2>&1
+fi
+
+printf "👷 Starting filebrowser in background... \n\n"
+
+
 printf "📂 Serving $${ROOT_DIR} at http://localhost:${PORT} \n\n"
 
-printf "Running 'filebrowser --noauth --root $ROOT_DIR --port ${PORT}$${DB_FLAG} --baseurl ${SERVER_BASE_PATH}' \n\n"
+printf "Running 'filebrowser $DB_FLAG' \n\n"
 
-filebrowser --noauth --root $ROOT_DIR --port ${PORT}$${DB_FLAG} --baseurl ${SERVER_BASE_PATH} > ${LOG_PATH} 2>&1 &
+filebrowser $DB_FLAG > ${LOG_PATH} 2>&1 &
 
 printf "📝 Logs at ${LOG_PATH} \n\n"
