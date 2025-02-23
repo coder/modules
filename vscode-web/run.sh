e7af502492fcd76df153c5956ce70b6776a0280a
Hao <14084342+Ricky-Hao@users.noreply.github.com>
2025-02-23 23:40:31 +0800

    Update run.sh

--- vscode-web/run.sh
+++ vscode-web/run.sh
@@ -20,7 +20,8 @@ fi
 run_vscode_web() {
   echo "👷 Running $VSCODE_CLI serve-web $EXTENSION_ARG $SERVER_BASE_PATH_ARG --port ${PORT} --host 127.0.0.1 --accept-server-license-terms --without-connection-token in the background..."
   echo "Check logs at ${LOG_PATH}!"
-  "$VSCODE_CLI" serve-web "$EXTENSION_ARG" "$SERVER_BASE_PATH_ARG" --port "${PORT}" --host 127.0.0.1 --accept-server-license-terms --without-connection-token > "${LOG_PATH}" 2>&1 &
+  # Todo: Add EXTENSION_ARG and SERVER_BASE_PATH_ARG
+  "$VSCODE_CLI" serve-web --port "${PORT}" --host 127.0.0.1 --accept-server-license-terms --without-connection-token > "${LOG_PATH}" 2>&1 &
 }
 
 # Check if the settings file exists...
