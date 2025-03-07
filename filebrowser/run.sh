36450ff1212c88cbfb0e9d2bb36383a7ea38544e
Dan Mills <evilhamsterman@gmail.com>
2025-03-06 19:53:16 -0800

    Use FB_DATABASE instead of DB_FLAG

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -16,26 +16,22 @@ printf "🛠️  Configuring filebrowser \n\n"
 ROOT_DIR=${FOLDER}
 ROOT_DIR=$${ROOT_DIR/\~/$HOME}
 
-DB_FLAG=""
-if [[ "${DB_PATH}" != "filebrowser.db" ]]; then
-  DB_FLAG=" -d ${DB_PATH}"
-fi
+echo "DB_PATH: ${DB_PATH}"
+
+export FB_DATABASE="${DB_PATH}"
 
 # Check if filebrowser db exists
-if [[ ! -f ${DB_PATH} ]]; then
-  filebrowser $DB_FLAG config init  2>&1 | tee -a ${LOG_PATH}
-  filebrowser $DB_FLAG users add admin "" --perm.admin=true --viewMode=mosaic 2>&1 | tee -a ${LOG_PATH} 
+if [[ ! -f "${DB_PATH}" ]]; then
+  filebrowser config init  2>&1 | tee -a ${LOG_PATH}
+  filebrowser users add admin "" --perm.admin=true --viewMode=mosaic 2>&1 | tee -a ${LOG_PATH} 
 fi
 
-filebrowser $DB_FLAG config set --baseurl=${SERVER_BASE_PATH} --port=${PORT} --auth.method=noauth --root=$ROOT_DIR 2>&1 | tee -a ${LOG_PATH} 
+filebrowser config set --baseurl=${SERVER_BASE_PATH} --port=${PORT} --auth.method=noauth --root=$ROOT_DIR 2>&1 | tee -a ${LOG_PATH} 
 
 printf "👷 Starting filebrowser in background... \n\n"
 
-
 printf "📂 Serving $${ROOT_DIR} at http://localhost:${PORT} \n\n"
 
-printf "Running 'filebrowser %s' \n\n" "$DB_FLAG"
-
-filebrowser $DB_FLAG >> ${LOG_PATH} 2>&1 &
+filebrowser >> ${LOG_PATH} 2>&1 &
 
 printf "📝 Logs at ${LOG_PATH} \n\n"
