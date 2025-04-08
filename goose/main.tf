55073da02618c23f1a993eb5773ee758a967f238
Ben Potter <ben@coder.com>
2025-04-08 02:03:10 +0000

    add debug

--- goose/main.tf
+++ goose/main.tf
@@ -204,7 +204,14 @@ EOL
       screen -U -dmS goose bash -c "
         cd ${var.folder}
         export PATH=\"$PATH_FOR_SCREEN\"
-        \"$GOOSE_CMD\" run --text \"$GOOSE_SYSTEM_PROMPT. Your task: $GOOSE_TASK_PROMPT\" --interactive | tee -a \"$HOME/.goose.log\"
+        echo \"Starting goose with command: $GOOSE_CMD\" | tee -a \"$HOME/.goose.log\"
+        echo \"Current PATH: $PATH\" | tee -a \"$HOME/.goose.log\"
+        echo \"Current directory: $(pwd)\" | tee -a \"$HOME/.goose.log\"
+        echo \"Goose version: $(\"$GOOSE_CMD\" --version 2>&1)\" | tee -a \"$HOME/.goose.log\"
+        echo \"Running goose command...\" | tee -a \"$HOME/.goose.log\"
+        \"$GOOSE_CMD\" run --text \"$GOOSE_SYSTEM_PROMPT. Your task: $GOOSE_TASK_PROMPT\" --interactive 2>&1 | tee -a \"$HOME/.goose.log\"
+        EXIT_CODE=\$?
+        echo \"Goose exited with code: \$EXIT_CODE\" | tee -a \"$HOME/.goose.log\"
         exec bash
       "
     else
@@ -253,7 +260,7 @@ resource "coder_app" "goose" {
         screen -xRR goose
       else
         echo "Starting a new Goose session." | tee -a "$HOME/.goose.log"
-        screen -S goose bash -c "export LANG=en_US.UTF-8; export LC_ALL=en_US.UTF-8; \"$GOOSE_CMD\" run --text \"Always report status and instructions to Coder, before and after your steps\" --interactive | tee -a \"$HOME/.goose.log\"; exec bash"
+        screen -S goose bash -c "export LANG=en_US.UTF-8; export LC_ALL=en_US.UTF-8; echo \"Starting goose with command: $GOOSE_CMD\" | tee -a \"$HOME/.goose.log\"; echo \"Current PATH: $PATH\" | tee -a \"$HOME/.goose.log\"; echo \"Current directory: $(pwd)\" | tee -a \"$HOME/.goose.log\"; echo \"Goose version: $(\"$GOOSE_CMD\" --version 2>&1)\" | tee -a \"$HOME/.goose.log\"; echo \"Running goose command...\" | tee -a \"$HOME/.goose.log\"; \"$GOOSE_CMD\" run --text \"Always report status and instructions to Coder, before and after your steps\" --interactive 2>&1 | tee -a \"$HOME/.goose.log\"; EXIT_CODE=\$?; echo \"Goose exited with code: \$EXIT_CODE\" | tee -a \"$HOME/.goose.log\"; exec bash"
       fi
     else
       cd ${var.folder}
