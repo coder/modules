9a0af2c4abf5debf67aaf67d4fc8152a748518b9
defelmnq <defelmnq+github@gmail.com>
2025-04-17 16:00:14 +0200

    add PNPM_HOME on pnpm

--- devcontainers-cli/run.sh
+++ devcontainers-cli/run.sh
@@ -12,12 +12,12 @@ if ! command -v docker > /dev/null 2>&1; then
 fi
 
 # Determine the package manager to use: npm, pnpm, or yarn
-if command -v pnpm > /dev/null 2>&1; then
-  PACKAGE_MANAGER="pnpm"
-elif command -v yarn > /dev/null 2>&1; then
+if command -v yarn > /dev/null 2>&1; then
   PACKAGE_MANAGER="yarn"
 elif command -v npm > /dev/null 2>&1; then
   PACKAGE_MANAGER="npm"
+elif command -v pnpm > /dev/null 2>&1; then
+  PACKAGE_MANAGER="pnpm"
 else
   echo "ERROR: No supported package manager (npm, pnpm, yarn) is installed. Please install one first." 1>&2
   exit 1
@@ -26,8 +26,15 @@ fi
 echo "Installing @devcontainers/cli using $PACKAGE_MANAGER..."
 
 # Install @devcontainers/cli using the selected package manager
-if [ "$PACKAGE_MANAGER" = "npm" ] || [ "$PACKAGE_MANAGER" = "pnpm" ]; then
-  $PACKAGE_MANAGER install -g @devcontainers/cli \
+if [ "$PACKAGE_MANAGER" = "npm" ]; then
+    $PACKAGE_MANAGER install -g @devcontainers/cli \
+    && echo "🥳 @devcontainers/cli has been installed into $(which devcontainer)!"
+elif [ "$PACKAGE_MANAGER" = "pnpm" ]; then
+    # if PNPM_HOME is not set, set it to the bin directory of the script
+    if [ -z "$PNPM_HOME" ]; then
+        export PNPM_HOME="$CODER_SCRIPT_BIN_DIR"
+    fi
+    $PACKAGE_MANAGER add -g @devcontainers/cli \
     && echo "🥳 @devcontainers/cli has been installed into $(which devcontainer)!"
 elif [ "$PACKAGE_MANAGER" = "yarn" ]; then
   $PACKAGE_MANAGER global add @devcontainers/cli \
